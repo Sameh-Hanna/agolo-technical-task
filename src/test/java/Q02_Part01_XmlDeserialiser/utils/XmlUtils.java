46783abe5ae151c9eb751feb17d26598ea025972
Sameh Hanna <sameh.hanna@outlook.com>
2024-04-27 14:03:16 +0300

    Adding new attributes to XML file

--- src/test/java/Q02_Part01_XmlDeserialiser/utils/XmlUtils.java
+++ src/test/java/Q02_Part01_XmlDeserialiser/utils/XmlUtils.java
@@ -7,6 +7,19 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.Transformer;
+
+import org.w3c.dom.Document;
+import java.io.*;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 public class XmlUtils {
     public static String readXmlFile(String filePath) throws IOException {
         StringBuilder xmlContent = new StringBuilder();
@@ -21,5 +34,45 @@ public class XmlUtils {
 
     public static JSONObject convertXmlToJson(String xmlContent) throws JSONException {
         return XML.toJSONObject(xmlContent);
+    }
+
+    public static void addAttribute(String filePath, String elementName, String elementValue, String updatedFilePath) throws IOException {
+        try {
+            // Create a document builder factory
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            // Create a document builder
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            // Parse the XML file into a document
+            Document document = builder.parse(new File(filePath));
+
+            // Get the root element of the document
+            Element rootElement = document.getDocumentElement();
+
+            // Get a NodeList of all <Website> elements
+            NodeList websiteList = rootElement.getElementsByTagName("Website");
+
+            // Iterate through each <Website> element
+            for (int i = 0; i < websiteList.getLength(); i++) {
+                // Access the item at index i
+                Node websiteNode = websiteList.item(i);
+                if (websiteNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element websiteElement = (Element) websiteNode;
+                    
+                    // Create new <Category> and <Owner> elements
+                    Element newElement = document.createElement(elementName);
+                    newElement.setTextContent(elementValue);
+
+                    websiteElement.appendChild(newElement);
+                }
+            }
+
+            // Save the modified document back to the XML file
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = (Transformer) transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(document);
+            StreamResult result = new StreamResult(new File(updatedFilePath));
+            transformer.transform(source, result);
+        }
+        catch(Exception e) {}
     }
 }
--- src/test/java/Q02_Part01_XmlDeserialiser/websitesXmlDeserializer.java
+++ src/test/java/Q02_Part01_XmlDeserialiser/websitesXmlDeserializer.java
@@ -3,11 +3,13 @@ package Q02_Part01_XmlDeserialiser ;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+
 import org.json.JSONObject;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
 import Q02_Part01_XmlDeserialiser.include.Website;
 import Q02_Part01_XmlDeserialiser.utils.XmlUtils;
+
 
 public class websitesXmlDeserializer {
 
@@ -16,9 +18,9 @@ public class websitesXmlDeserializer {
     public static void main(String[] args) {
         // Specify the relative path to the XML file
         String xmlFilePath = "src/test/java/Q02_Part01_XmlDeserialiser/xml_file/websites.xml";
+        String updatedXmlFilePath = "src/test/java/Q02_Part01_XmlDeserialiser/xml_file/updated_websites.xml";
 
         try {
-
             XmlMapper xmlMapper = new XmlMapper();  
 
             // Read the XML file
@@ -28,7 +30,7 @@ public class websitesXmlDeserializer {
             List<Website> websites = xmlMapper.readValue(xmlContent, xmlMapper.getTypeFactory().constructCollectionType(List.class, Website.class));
 
             // Print deserilised website objects
-            System.out.println("Websites list:");
+            System.out.println("Deserilised Websites xml list:");
             for (int i = 0; i < websites.size(); i++) {
                 Website website = websites.get(i);
                 System.out.println("Website " + (i + 1) + ":");
@@ -36,12 +38,24 @@ public class websitesXmlDeserializer {
             }
             System.out.println();
 
+            // Add new attributes to XML file
+            XmlUtils.addAttribute(xmlFilePath, "Category", "default_category", updatedXmlFilePath);
+            XmlUtils.addAttribute(updatedXmlFilePath, "Owner", "default_owner", updatedXmlFilePath);
+
+            // Read the updated XML file
+            String updatedXmlContent = XmlUtils.readXmlFile(updatedXmlFilePath);
+            
+            // Print XML after adding the new attributes (Owner & Category)
+            System.out.println("XML file Content with the new added attributes (Owner & Category):");
+            System.out.println(updatedXmlContent);
+            System.out.println();
+
             // Convert XML content to JSON
-            JSONObject jsonObj = XmlUtils.convertXmlToJson(xmlContent);
+            JSONObject jsonObj = XmlUtils.convertXmlToJson(updatedXmlContent);
             String jsonPrettyPrintString = jsonObj.toString(PRETTY_PRINT_INDENT_FACTOR);
             
             // Print Websites JSON object
-            System.out.println("Websites JSON object:");
+            System.out.println("Print converted XML to JSON:");
             System.out.println(jsonPrettyPrintString);
             System.out.println();
 
@@ -52,7 +66,7 @@ public class websitesXmlDeserializer {
             List<Website> filteredWebsites = Website.filterWebsitesByCreatedDate(websites, filterCreatedDate);
 
             // Print the filtered websites' information
-            System.out.println("Filtered Websites list:");
+            System.out.println("Print filtered Websites list:");
             Website.printWebsites(filteredWebsites);
 
         } catch (Exception e) {
